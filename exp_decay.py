ad9640ea20b849885d5aabc4f7e1ce8f92bffd20
Stian Berg Hansen <stiabh@student.matnat.uio.no>
2020-09-14 10:07:47 +0200

    Completed 1b). Implemented solver

--- exp_decay.py
+++ exp_decay.py
@@ -1,3 +1,5 @@
+import numpy as np
+import matplotlib.pyplot as plt
 from scipy.integrate import solve_ivp
 
 
@@ -8,3 +10,25 @@ class ExponentialDecay:
     def __call__(self, t, u):
         """Return RHS of ODE."""
         return -self.a*u
+
+    def solve(self, u0, T, dt):
+        """Solve ODE for initial condition u0 on (0, T], in steps of dt.
+        Returns two arrays, one for time points and one for solution points.
+        """
+        _t_eval = np.arange(0, T+dt, dt)
+        _sol = solve_ivp(self.__call__, (0, T), (u0, ), t_eval=_t_eval)
+        return _sol.t, _sol.y[0]
+
+
+if __name__ == "__main__":
+    a = 0.4
+    u0 = 3.2
+    T = 20
+    dt = 0.1
+
+    decay_model = ExponentialDecay(a)
+    t, u = decay_model.solve(u0, T, dt)
+
+    plt.plot(t, u, label=r"$a = 0.4, u_0 = 3.2$")
+    plt.legend()
+    plt.show()
