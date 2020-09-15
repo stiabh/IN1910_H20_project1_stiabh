8f2eab9c33c37f6826bdebd5ae77898b6c64fc5f
Stian Berg Hansen <stiabh@student.matnat.uio.no>
2020-09-15 10:31:49 +0200

    Completed 3c). Added method solve, based on method from class Pendulum

--- double_pendulum.py
+++ double_pendulum.py
@@ -1,3 +1,9 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from numpy import cos, sin, pi
+from scipy.integrate import solve_ivp
+
+
 class DoublePendulum:
     def __init__(self, M1=1, L1=1, M2=1, L2=1, g=9.81):
         """Set parameters of pendulums.
@@ -20,10 +26,8 @@ class DoublePendulum:
 
     def __call__(self, t, y):
         """Return RHS of ODEs."""
-        theta1 = y[0]
-        omega1 = y[1]
-        theta2 = y[2]
-        omega2 = y[3]
+        theta1, omega1, theta2, omega2 = y[0], y[1], y[2], y[3]
+        M1, L1, M2, L2 = self.M1, self.L1, self.M2, self.L2
         g = self.g
         delta_t = theta2 - theta1
 
@@ -42,3 +46,29 @@ class DoublePendulum:
                                                - M2*L2*cos(delta_t)**2)
 
         return dtheta1_dt, domega1_dt, dtheta2_dt, domega2_dt
+
+    def solve(self, y0, T, dt, angles="rad"):
+        """Solve set of ODEs using scipy.
+
+        y0: Tuple (theta1_0, omega1_0, theta2_0, omega2_0) with 
+            initial conditions
+        T: End of interval (0, T)
+        dt: Time step for interval
+        angles: deg (degrees) or rad (radians)
+        """
+        theta1_0, omega1_0, theta2_0, omega2_0 = y0[0], y0[1], y0[2], y0[3]
+
+        if angles == "rad":
+            pass
+        elif angles == "deg":
+            theta1_0 = theta1_0*pi/180
+            omega1_0 = omega1_0*pi/180
+            theta2_0 = theta2_0*pi/180
+            omega2_0 = omega2_0*pi/180
+        else:
+            raise ValueError("Angles must be set to deg or rad")
+
+        _t_eval = np.arange(0, T+dt, dt)
+        _sol = solve_ivp(self.__call__, (0, T), (theta1_0, omega1_0, 
+                         theta2_0, omega2_0), t_eval=_t_eval)
+        self._t, self._y = _sol.t, _sol.y
